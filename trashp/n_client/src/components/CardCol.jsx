d79c70737beb1944410a073f4cbd0650a1b476d5
rksvw <ritik.vw98@gmail.com>
2024-04-22 15:27:06 +0530

    product cards completed

--- trashp/n_client/src/components/CardCol.jsx
+++ trashp/n_client/src/components/CardCol.jsx
@@ -1,14 +1,43 @@
-import { Fragment } from 'react'
-import Releases from './Releases'
-import Soon from './Soon'
+import { Fragment, useEffect, useState } from "react";
+import Releases from "./Releases";
+import Soon from "./Soon";
+import "./release.css";
+
+const api_key = "8e5ab8c0d90445c49714b423bf53c004";
+const query = "popular in 2024";
+const url = `https://api.rawg.io/api/games?search=${query}&key=${api_key}`;
 
 function CardCol() {
+  const [gameData, setGameData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const gameAPI = async () => {
+    try {
+      const res = await fetch(url);
+      const jsonData = await res.json();
+      return jsonData;
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    gameAPI().then((data) => {
+      setGameData(data);
+      setLoading(false);
+    });
+  }, []);
+
   return (
     <Fragment>
-    <Releases />\
-    <Soon />
+    {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <Releases data={gameData} />
+      )}
+      <Soon />
     </Fragment>
-  )
+  );
 }
 
-export default CardCol
+export default CardCol;
--- trashp/n_client/src/components/Releases.jsx
+++ trashp/n_client/src/components/Releases.jsx
@@ -1,11 +1,26 @@
-import { Fragment } from 'react';
+import { Fragment } from "react";
+import "./release.css";
 
-function Releases() {
+function Releases({ data }) {
+  // const game = this.props.gameData
   return (
     <Fragment>
-
+      <div className="game-panel">
+        {data.results.map((gm, index) => {
+          return (
+            <div key={index} className="cards">
+              <img
+                src={gm.background_image}
+                alt={gm.name}
+                className="card-img"
+              />
+              <div className="card-title"><h4>{gm.name}</h4></div>
+            </div>
+          );
+        })}
+      </div>
     </Fragment>
-  )
+  );
 }
 
-export default Releases
+export default Releases;
